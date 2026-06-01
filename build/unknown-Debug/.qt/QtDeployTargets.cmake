9d20dc081de7f4a43fbc9cc307415e5f3561db22
Lenci <1780008573@qq.com>
2026-06-01 08:33:00 +0800

    chore: remove build artifacts

--- build/unknown-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_QtPractice_FILE /Users/enenci/QtPractice/build/unknown-Debug/QtPractice.app/Contents/MacOS/QtPractice)
-set(__QT_DEPLOY_TARGET_QtPractice_TYPE EXECUTABLE)
--- build/unknown-Debug/.qt/deploy_QtPractice_b236e25a18.cmake
+++
@@ -1,7 +0,0 @@
-include("/Users/enenci/QtPractice/build/unknown-Debug/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/QtPractice-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "QtPractice.app"
-)
